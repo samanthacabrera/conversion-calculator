9c65966c85640bedc5027c757d5ea6bef425f722
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-10-03 09:13:07 -0700

    added basic header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header';
 import Converter from './Converter';
 
 const App = () => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-2xl my-12">Unit Conversion Calculator</h1>
+      <Header/>
       <Converter />
       <a href="https://github.com/samanthacabrera" target="_blank" rel="noopener noreferrer" className="absolute bottom-0 right-0 pr-1">
         made by <span className="hover:underline hover:italic">sam cabrera</span>
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <h1 className="text-2xl tracking-wide my-12">Unit Conversion Calculator</h1>
+    )
+}
+
+export default Header
