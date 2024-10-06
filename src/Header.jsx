b840c945fd41ed75d8725c4aa820b32a9666a19f
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-10-06 15:21:04 -0600

    updated title

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <div>
-            <h1 className="text-2xl tracking-wide lowercase font-semibold p-2 my-16 lg:my-2 lg:mt-20 hover:scale-105 duration-200">
+            <h1 className="text-4xl tracking-wide lowercase p-2 my-16 lg:my-2 lg:mt-20 hover:scale-105 duration-200">
                 Calcalooza
             </h1>
         </div>
